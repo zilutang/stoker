84e975b785cb4436ba6a33414df076ea4e2d0661
tangzilu <tangzilu@tangziludeMBP.4pd.io>
2017-09-13 10:43:38 +0800

    update sending email

---
+++ mail.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 09:40:13 2017
+
+@author: tangzilu
+"""
+
+import commands
+import pandas as pd
+import os
+#exit()
+#发送文本信息
+#commands.getstatusoutput('mail -s "testing message" 184083376@qq.com <<< "body from python"')
+#添加附件发送/
+alltodaypd = pd.read_csv("/data/codes/stoker/resources/daily/score.csv")
+todayScorepd = alltodaypd[alltodaypd.columns[-3:]]
+todayColumn = todayScorepd.columns[0]
+todayScorepd = todayScorepd.sort_values(by=todayColumn, ascending=True).reset_index()
+todayScoreNewpd = todayScorepd[[todayColumn, "name", "scoreToday"]]
+todayScoreNewpd.to_csv("/data/codes/stoker/resources/daily/score.txt")
+
+#todayScoreText = todayScoreText.
+commands.getstatusoutput('/data/codes/stoker/resources/daily/sending.sh')
+#os.popen('echo "This is with attach" | mail -s "subject" 184083376@qq.com -A /data/codes/stoker/resources/daily/score.txt')
---
+++ sending.sh
@@ -0,0 +1,1 @@
+echo "This is with attach" | mail -s "subject" 184083376@qq.com -A /data/codes/stoker/resources/daily/score.txt
