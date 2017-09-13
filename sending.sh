cffa2cd183425d1188971bd9084763bf727b90f9
tangzilu <tangzilu@tangziludeMBP.4pd.io>
2017-09-13 20:26:24 +0800

    update files

--- sending.sh
+++ sending.sh
@@ -2,5 +2,5 @@ python /data/codes/stoker/updateDaily.py
 python /data/codes/stoker/scoring.py
 python /data/codes/stoker/mail.py
 
-echo "scoring for stocks" | mail -s "subject" 184083376@qq.com -A /data/codes/stoker/resources/daily/score.txt
-echo "scoring for stocks" | mail -s "subject" 184083376@qq.com -A /data/codes/stoker/resources/daily/alltoday.xlsx
+echo "score.txt" | mail -s "score.txt" 184083376@qq.com -A /data/codes/stoker/resources/daily/score.txt
+echo "alltoday.xlsx" | mail -s "alltoday" 184083376@qq.com -A /data/codes/stoker/resources/daily/alltoday.xlsx
